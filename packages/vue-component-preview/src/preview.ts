7f6e463a63b85360a381b2abab338639920c6013
Tao Wen <taowen@chengfayun.com>
2022-03-18 19:10:46 +0800

    simplify

--- packages/vue-component-preview/src/preview.ts
+++ packages/vue-component-preview/src/preview.ts
@@ -11,17 +11,14 @@ export function preview(app: any, dataProvider: any) {
             const ctx = {};
             const wrappedComponents = {};
             const counters: Record<string, number> = {};
-            if (app.components) {
-                for (const [componentName, componentType] of Object.entries(app.components)) {
-                    wrappedComponents[componentName] = preview(componentType, (props) => {
-                        const counter = counters[componentName] = counters[componentName] || 0;
-                        counters[componentName]++;
-                        if (data.__render__) {
-                            return data.__render__({ counter, componentName, componentType }, props);
-                        }
-                        return undefined;
-                    });
-                }
+            for (const [componentName, componentType] of Object.entries(app.components || {})) {
+                wrappedComponents[componentName] = preview(componentType, (props) => {
+                    const counter = counters[componentName] = counters[componentName] || 0;
+                    counters[componentName]++;
+                    if (data.__render__) {
+                        return data.__render__({ counter, componentName, componentType }, props);
+                    }
+                });
             }
             this.$.components = wrappedComponents;
             this.$.directives = app.directives;
@@ -35,20 +32,11 @@ export function preview(app: any, dataProvider: any) {
 }
 
 function wrapProxy(data: any) {
-    if (!data) {
-        return data;
-    }
-    if (typeof data !== 'object') {
-        return data;
-    }
+    if (typeof data !== 'object') { return data; }
     return new Proxy({}, {
         get(target, p, receiver) {
-            if (data[p]) {
-                return wrapProxy(data[p]);
-            }
-            if (data.__get__) {
-                return wrapProxy(data.__get__(p));
-            }
+            if (data[p]) { return wrapProxy(data[p]); }
+            if (data.__get__) { return wrapProxy(data.__get__(p)); }
         }
     })
 }
